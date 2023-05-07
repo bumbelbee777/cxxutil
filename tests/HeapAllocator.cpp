23862c99b39aa00b38b870fc6c70444d2c2f0ce9
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2023-05-07 13:51:30 +0100

    Forgot to include unit test.

--- tests/HeapAllocator.cpp
+++ tests/HeapAllocator.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Ali on 5/4/2023.
-//
+#include <All.h>
+#include <iostream>
+#include <cassert>
+
+int main(int argc, char **argv) {
+	void *MyPointer = Heap.Allocate(100);
+	assert(MyPointer, "Heap.Allocate() failed!");
+	Heap.Free(MyPointer);
+	std::cout << "Freed MyPointer." << '\n';
+	void *MyPointer2 = Heap.AllocateZeroed(200);
+	assert(MyPointer2, "Heap.AllocateZeroed() failed!");
+	MyPointer2 = Heap.ReAllocate(300);
+	assert(MyPointer2, "Heap.ReAllocate() failed!");
+	Heap.Free(MyPointer2);
+	std::cout << "All tests passed successfully!" << '\n';
+	return 0;
+}
